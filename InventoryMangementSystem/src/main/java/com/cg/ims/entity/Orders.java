6903f0edb868bc54f8402c78d251f9f2191eb6cd
Barnwal <aryan-kumar.barnwal@capgemini.com>
2024-12-20 09:36:54 +0530

    Orders inventory created

---
+++ InventoryMangementSystem/src/main/java/com/cg/ims/entity/Orders.java
@@ -0,0 +1,40 @@
+package com.cg.ims.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "orders")
+public class Orders {
+	
+	
+	@Id
+	@Column(name = "order_id")
+	private int orderID;
+	
+	@Column(name = "order_tms")
+	private LocalDateTime orderTms;
+	
+	@ManyToOne
+	@JoinColumn(name = "customer_id", referencedColumnName = "customer_id")
+	private Customers customer;
+	
+	@Column(name = "order_status")
+	private String orderStatus;
+	
+	@ManyToOne
+	@JoinColumn(name = "store_id", referencedColumnName = "store_id")
+	private Stores store;
+}
