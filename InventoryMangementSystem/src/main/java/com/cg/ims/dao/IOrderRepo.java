5d7b943a0a0fde3ec2e1ac041001e0f828d5f25c
Barnwal <aryan-kumar.barnwal@capgemini.com>
2024-12-20 20:26:41 +0530

    created a Order repository

---
+++ InventoryMangementSystem/src/main/java/com/cg/ims/dao/IOrderRepo.java
@@ -0,0 +1,11 @@
+package com.cg.ims.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cg.ims.entity.Orders;
+
+@Repository
+public interface IOrderRepo extends JpaRepository<Orders, Integer> {
+
+}
